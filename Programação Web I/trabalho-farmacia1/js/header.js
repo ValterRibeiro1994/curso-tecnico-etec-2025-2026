c5257784e48006320819e55da5e78b86e63117c3
ValterRibeiro1994 <valtertert@gmail.com>
2025-11-11 21:00:16 -0300

    Reavaliando projeto, planejamento elaborado

---
+++ Programação Web I/trabalho-farmacia1/js/header.js
@@ -0,0 +1,9 @@
+// Linkar o banner para que ele sempre volte para o index do site
+const img_banner = document.getElementById("img-banner");
+
+// adicionar o evento de clique
+img_banner.addEventListener("click", function(event
+){
+    window.location.href = "index.html";
+    alert("Ok");
+})
